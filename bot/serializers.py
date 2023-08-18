1ed7d5764298df1f5f86a97d252a624178d10aaf
namazovanton <namazovanton@gmail.com>
2023-08-19 00:29:15 +0300

    hw_38(bot)_serializer_and_view

--- bot/serializers.py
+++ bot/serializers.py
@@ -1,16 +1,11 @@
-# from rest_framework import serializers
-# from bot.models import TgUser
-# from core.models import User
-#
-#
-# class TgUserVerificationSerializer(serializers.ModelSerializer):
-#     def validate_verification_code(self, verification_code: str) -> str:
-#         if not TgUser.objects.filter(
-#                 verification_code=verification_code).exists():
-#             raise serializers.ValidationError("Код неверный")
-#         return verification_code
-#
-#     class Meta:
-#         id = serializers.IntegerField(read_only=True)
-#         model = User
-#         fields = ['verification_code']
+from rest_framework import serializers
+from bot.models import TgUser
+
+
+class TgUserSerializer(serializers.ModelSerializer):
+    # tg_id = serializers.IntegerField(source="chat_id", read_only=True)
+    # username = serializers.CharField(source="user.username", read_only=True)
+
+    class Meta:
+        model = TgUser
+        fields = ['verification_code']
--- bot/views.py
+++ bot/views.py
@@ -1,29 +1,29 @@
-# from django.shortcuts import render
-# from rest_framework import generics, status, response
-# from rest_framework.permissions import IsAuthenticated
-#
-# from bot.management.commands.run_bot import Command
-# from bot.models import TgUser
-# from bot.serializers import TgUserVerificationSerializer
-# from core.models import User
-#
-#
-# class TgUserVerification(generics.CreateAPIView):
-#     queryset = User
-#     serializer_class = TgUserVerificationSerializer
-#     permission_classes = [IsAuthenticated]
-#
-#     def get_object(self) -> User:
-#         return self.request.user
-#
-#     def perform_update(self, serializer):
-#         tg_client = Command.tg_client
-#         verification_code = self.request.data["verification_code"]
-#         tg_user = TgUser.objects.get(verification_code=verification_code)
-#         user = self.get_object()
-#         tg_user.user_id = user
-#         tg_user.save()
-#         user.verification_code = tg_user.verification_code
-#         user.save()
-#         tg_client.send_message(chat_id=tg_user.tg_chat_id, text=answer)
-#         return response.Response("Verification completed successfully", status=status.HTTP_200_OK)
+from typing import Any
+
+from rest_framework import generics, status, response
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.request import Request
+from rest_framework.response import Response
+from rest_framework.exceptions import bad_request
+
+from bot.models import TgUser
+from bot.serializers import TgUserSerializer
+from bot.tg.client import TgClient
+
+
+class TgUserVerification(generics.UpdateAPIView):
+    serializer_class = TgUserSerializer
+    permission_classes = [IsAuthenticated]
+    queryset = TgUser.objects.all()
+
+    def patch(self, request: Request, *args: Any, **kwargs: Any) -> Response:
+        try:
+            tg_user = self.get_queryset().get(verification_code=request.data.get('verification_code'))
+        except TgUser.DoesNotExist:
+            raise bad_request
+
+        tg_user.user = request.user
+        tg_user.save()
+        TgClient.send_message(chat_id=tg_user.chat_id, text='Bot verification completed successfully')
+
+        return Response(TgUserSerializer(tg_user).data)
